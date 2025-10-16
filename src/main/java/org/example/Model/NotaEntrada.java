96f139691a370633822c0b5d34ff5b515ed80cde
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2025-10-16 19:34:58 -0300

    feat: Cria model NotaEntrada

---
+++ src/main/java/org/example/Model/NotaEntrada.java
@@ -0,0 +1,49 @@
+package org.example.Model;
+
+import java.time.LocalDate;
+
+public class NotaEntrada {
+
+    private int id;
+    private int idFornecedor;
+    private LocalDate dataEntrada;
+
+    public NotaEntrada(int id, int idFornecedor, LocalDate dataEntrada) {
+        this.id = id;
+        this.idFornecedor = idFornecedor;
+        this.dataEntrada = dataEntrada;
+    }
+
+    public NotaEntrada(LocalDate dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdFornecedor() {
+        return idFornecedor;
+    }
+
+    public void setIdFornecedor(int idFornecedor) {
+        this.idFornecedor = idFornecedor;
+    }
+
+    public LocalDate getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(LocalDate dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    @Override
+    public String toString() {
+        return "NotaEntrada[" + "id=" + id + ", idFornecedor=" + idFornecedor + ", dataEntrada=" + dataEntrada + ']';
+    }
+}
